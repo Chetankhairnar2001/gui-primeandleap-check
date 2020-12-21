696876df0d226bd32a0b31894953ec44ba7c4586
ChetanKhairnar <chetan.khairnar2001@gmail.com>
2020-12-21 13:33:09 +0530

    feat: add new output text and wire code

--- add.js
+++ add.js
@@ -1,10 +1,16 @@
 var input1=document.querySelector("#inputtext1");
 var output1=document.querySelector("#output1");
+var output2=document.querySelector("#output2");
 var inpbutton=document.querySelector("#inputbtn");
 
 function play(){
-    var value=input1.value;
-    console.log(value);
+    var value=input1.value.split("/");
+    output1.innerText=checkprime(value[0]);
+    output2.innerText=value[2];
 }
 
-inpbutton.addEventListener("click",play());
+function checkprime(input){
+    return input;
+}
+
+inpbutton.addEventListener("click",play);
